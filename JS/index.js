943bd7b8ccec2f9baced2e202ef7e8612ade66bb
Yaqoob <sualehfarooq65@gmail.com>
2022-11-11 21:52:08 -0800

    Commit
    Boiler Plate created with firebase config

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBer4IUmkGuMMkLWsRD4sxPuWV5RQUi0cI",
+    authDomain: "attendance-portal-33cdd.firebaseapp.com",
+    projectId: "attendance-portal-33cdd",
+    storageBucket: "attendance-portal-33cdd.appspot.com",
+    messagingSenderId: "750134453831",
+    appId: "1:750134453831:web:ae9fd8be9af93cdb4f7362"
+  };
+
+  const app = initializeApp(firebaseConfig);
